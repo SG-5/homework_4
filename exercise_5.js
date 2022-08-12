617e74dc5b725df45b08d85d49c816acbda581d5
SG-5 <surengevorgyan5@gmail.com>
2022-08-12 21:37:09 +0400

    add exercise_5

--- exercise_5.js
+++ exercise_5.js
@@ -1,4 +1,72 @@
 export function OOP_Structure() {
+
+
+  class City {
+    
+    get buildings() {
+     return  this.building
+    }
+    
+    set buildings(build) {
+      this.building=build
+    }
+    
+  }
+  
+
+  class Building extends City {
+  
+    get car() {
+      return this.cars
+     }
+     
+     set car(cars) {
+       this.cars=cars
+     }
+
+
+  }
+class Hospital extends Building {
+  }
+  
+class PoliceDepartment extends Building {
+ 
+} 
+
+class Car extends Building{
+  
+} 
+  
+  
+class PoliceCar extends Car {
+
+}
+class AmbulanceCar extends Car{
+
+}
+
+
+  // const city = new City('nairiHospital', 'erebuniHospital', 'centralPoliceDepartment')
+  // const build= new Building('Educational Buildings')
+  // city.buildings 
+  // console.log( build)
+
+  const city = new City()
+  
+  const nairiHospital = new Hospital();
+  const erebuniHospital = new Hospital();
+  const centralPoliceDepartment = new PoliceDepartment()
+
+  city.buildings=[nairiHospital,erebuniHospital,centralPoliceDepartment]
+
+
+  const policeCar = new PoliceCar();
+  const toyota= new PoliceCar()
+  const ambulanceCar = new AmbulanceCar()
+
+  
+  nairiHospital.car=[ambulanceCar]
+
   
 
 
@@ -8,7 +76,9 @@ export function OOP_Structure() {
 
 
 
-
-
-
 }
+
+
+
+
+
